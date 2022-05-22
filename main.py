7495b7e56f3db4fcd178eb3443b271c67b7f2edd
Seungwoo Yoo <ryuryu10@naver.com>
2022-05-22 16:49:09 +0900

    키 입력 기능 모듈화

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 from pynput.keyboard import Key, Listener
-from module import window
-
-def func(key):
-    print(key)
-
-
-
+from module import window, keyboard
 
 if __name__ == "__main__":
-    with Listener(on_press=func) as listener:
-        window.Window_design()
+    with Listener(on_press=keyboard.func) as listener:
+        window.installation()
+        window.design()
         window.root.mainloop()
         listener.join()
-    
--- module/window.py
+++ module/window.py
@@ -1,10 +1,33 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 import tkinter.scrolledtext as tkst
+import ctypes
+import math
 
 root = tk.Tk()
 
-def Window_design():
+def installation():
+    User32 = ctypes.windll.user32
+    User_Screen_Middle_width = math.trunc((User32.GetSystemMetrics(0) / 2))
+    User_Screen_Middle_height = math.trunc((User32.GetSystemMetrics(1) / 2))
+    Program_ScreenSize_width = 1000
+    Program_ScreenSize_height = 800
+    MPSW = math.trunc(Program_ScreenSize_width / 2)
+    MPSH = math.trunc(Program_ScreenSize_height / 2)
+    Screen_Size = "{0}x{1}+{2}+{3}".format(
+        Program_ScreenSize_width,
+        Program_ScreenSize_height,
+        User_Screen_Middle_width-MPSW,
+        User_Screen_Middle_height-MPSH
+    )
+    root.geometry(Screen_Size)
+    root.resizable(False, False)
+    root.wm_attributes("-topmost", 1)
+    root.overrideredirect(True)
+    root.lift()
+
+
+def design():
     Frame1 = ttk.Frame(root)
     Frame1.place(relx=0.01, rely=0.013, relheight=0.975,relwidth=0.375)
     Frame1.configure(relief='groove')
